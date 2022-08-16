396785c31d52bad8ef2a00ee2f64322271010435
Swagnarok <darryl.le.630@gmail.com>
2022-08-15 19:51:20 -0700

    created first option logic, and checking for functionality

--- index.js
+++ index.js
@@ -2,7 +2,42 @@ const connection = require("./config/connection")
 const inquirer = require("inquirer")
 
 function start() {
-    console.log("we starting")
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    console.log("|                                                 |")
+    console.log("|               EMPLOYEE MANAGEMENT               |")
+    console.log("|                      SYSTEM                     |")
+    console.log("|                                                 |")
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+    userPrompt();
+}
+
+function userPrompt() {
+    inquirer.prompt({
+        type: "list",
+        name: "initialChoice",
+        message: "What would you like to do?",
+        choices: [
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add A Department",
+            "Add A Role",
+            "Add An Employee",
+            "Update An Employee Role"
+        ]
+    }).then(answer => {
+        switch(answer.initialChoice) {
+            case "View All Departments":
+            return viewDept();
+            default:
+        }
+    })
+}
+
+function viewDept() {
+    const view = connection.query(`SELECT * FROM department`)
+    console.table(view)
 }
 
 start();
